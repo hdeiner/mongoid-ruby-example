4ec443219a25f0b4089dcabc358e9b26ade4b945
Howard Deiner <howard.deiner@deinersoft.com>
2016-08-30 15:48:52 -0500

    demo snap/account query after inserts

--- rooExample/snap_data.rb
+++ rooExample/snap_data.rb
@@ -2,15 +2,6 @@ class SnapData
   include Mongoid::Document
 
   field :id, type: Integer
-  field :fname, type: String
-  field :lname, type: String
-  field :mname, type: String
-  field :dob, type: String
-  field :ssn, type: String
-  field :address, type: String
-  field :state, type: String
-  field :zip, type: String
-  field :homePhone, type: String
   field :referenceNumber, type: String
   field :statusCode, type: String
   field :statusMessage, type: String
